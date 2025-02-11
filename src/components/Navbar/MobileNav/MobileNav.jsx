0bad8d2600646435f67e285de482da4a534d359f
BAH Mamad <mbah@student.42lyon.fr>
2025-02-11 19:30:04 +0100

    feat(Navbar): add Navbar component with styles

---
+++ src/components/Navbar/MobileNav/MobileNav.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./MobileNav.css";
+
+const MobileNav = ({ isOpen, toggleMenu }) => {
+  
+  const handleScroll = (sectionId) => {
+    if(isOpen) toggleMenu();
+    document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <>
+      <div
+        className={`mobile-menu ${isOpen ? "active" : ""}`}
+        onClick={toggleMenu}
+      >
+        <div className="mobile-menu-container">
+          <img className="logo" src="./assets/images/logo.png" alt="" />
+
+          <ul>
+            <li>
+              <a onClick={() => handleScroll("hero")} className="menu-item">
+                Home
+              </a>
+            </li>
+
+            <li>
+              <a onClick={() => handleScroll("skills")} className="menu-item">
+                Skills
+              </a>
+            </li>
+
+			<li>
+              <a onClick={() => handleScroll("education")} className="menu-item">
+                Educations
+              </a>
+            </li>
+
+            <li>
+              <a onClick={() => handleScroll("work-exp")} className="menu-item">
+                Projects
+              </a>
+            </li>
+
+            <li>
+              <a onClick={() => handleScroll("contact")} className="menu-item">
+                Contact Me
+              </a>
+            </li>
+
+            <button className="contact-btn" onClick={() => {}}>
+              <a href="./assets/images/cv.pdf">Hire Me</a>
+            </button>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MobileNav;
